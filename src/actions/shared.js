48546f1805eff4c1c2e6f0669a4acb1d3eb205ab
Gilbert Nwankwo <kellyemeka7@gmail.com>
2020-03-14 09:28:11 -0400

    like a tweet feature complete

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { recieveUsers } from '../actions/users'
 import { recieveTweets } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
             .then(({ users, tweets }) => {
                 dispatch(recieveUsers(users))
                 dispatch(recieveTweets(tweets))
                 dispatch(setAuthedUser(AUTHED_ID))
+                dispatch(hideLoading())
             })
     }
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,7 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline } from 'react-icons/ti'
 import { TiHeartOutline } from 'react-icons/ti'
 import { TiHeartFullOutline } from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
 
@@ -15,7 +16,12 @@ class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
 
-        // todo: handleLink
+        const { dispatch, tweet, authedUser } = this.props
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     render() {
